750d7ec8d6bb5d824a9c022bfd1b52e4358a5913
AnTomS <thekeibee@yandex.ru>
2023-12-22 00:34:41 +0800

    Initial commit

---
+++ app/src/main/java/com/atom/searchcoffe/ui/AuthFragment.kt
@@ -0,0 +1,31 @@
+package com.atom.searchcoffe.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.atom.searchcoffe.databinding.FragmentAuthBinding
+
+class AuthFragment : Fragment() {
+
+    private var _binding: FragmentAuthBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAuthBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
